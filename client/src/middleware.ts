ab92d49ae9a745b29e3f2d946bfa289b75dd1678
Pratham <prathamvasani1@gmail.com>
2025-01-05 17:22:06 +0530

    added middleware for testing

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -7,14 +7,20 @@ export default (request: NextRequest) => {
   const pathname = request.nextUrl.pathname;
   const session = request.cookies.get('connect.sid');
 
-  console.log(session, request.cookies);
+  console.log(session, pathname, request.cookies, publicRoutes, privateRoutes);
+  console.log(privateRoutes.includes(pathname));
+  console.log(publicRoutes.includes(pathname));
 
   if (!session) {
-    if (privateRoutes.includes(pathname))
+    if (privateRoutes.includes(pathname)) {
+      console.log('here1');
       return NextResponse.redirect(new URL('/auth', request.nextUrl));
+    }
   } else {
-    if (publicRoutes.includes(pathname))
+    if (publicRoutes.includes(pathname)) {
+      console.log('here2');
       return NextResponse.redirect(new URL('/chat', request.nextUrl));
+    }
   }
 
   return NextResponse.next();
